eeb8ed20b3c4d54cf65fdd2ac45937c0df047866
latifbey <latifbey@yandex.com>
2025-02-04 16:30:11 +0100

    created parts of productservice

---
+++ productservice/src/main/java/com/ecommerce/productservice/model/Product.java
@@ -0,0 +1,20 @@
+package com.ecommerce.productservice.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "products")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String description;
+    private Double price;
+}
