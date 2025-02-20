0d2606a55c6e610b14ff3b97a097442df0517d98
latifbey <latifbey@yandex.com>
2025-02-20 17:55:27 +0100

    Fix CI/CD pipeline

--- productservice/src/test/java/com/ecommerce/productservice/ProductserviceApplicationTests.java
+++ productservice/src/test/java/com/ecommerce/productservice/ProductserviceApplicationTests.java
@@ -2,6 +2,7 @@ package com.ecommerce.productservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
